9f7e35cd1f768940f2dc24fd9d3107d181b51d33
root <root@UC80-409-8588.campus.net.ucsf.edu>
2020-03-17 08:46:39 -0700

    fixing bug

--- app.py
+++ app.py
@@ -25,9 +25,17 @@ known_infections = 53
 initial_infections = st.sidebar.number_input(
     "Currently Known Regional Infections", value=known_infections, step=10, format="%i"
 )
-current_hosp = st.sidebar.number_input(
-    "Currently Hospitalized COVID-19 Patients", value=2, step=1, format="%i"
-)
+
+#aa - added back
+detection_prob = (st.sidebar.number_input(
+    "Probability of Detection (%)", 0, 100, value=5, step=1, format="%i"
+)/ 100.0)
+
+#aa - deleted
+#current_hosp = st.sidebar.number_input(
+#    "Currently Hospitalized COVID-19 Patients", value=2, step=1, format="%i"
+#)
+
 doubling_time = st.sidebar.number_input(
     "Doubling Time (days)", value=6, step=1, format="%i"
 )
@@ -55,38 +63,24 @@ S = st.sidebar.number_input(
     "Regional Population", value=S_default, step=100000, format="%i"
 )
 
-hosp_capacity = st.sidebar.number_input(
-    "Hospital Capacity",  value=1000, step=100, format="%i"
-)
+#aa -changed
+#total_infections = current_hosp / Penn_market_share / hosp_rate
+#detection_prob = initial_infections / total_infections
+total_infections = initial_infections / detection_prob
 
-icu_capacity = st.sidebar.number_input(
-    "ICU Capacity",  value=300, step=50, format="%i"
-)
 
-vent_capacity = st.sidebar.number_input(
-    "Ventilator Capacity",  value=200, step=50, format="%i"
-)
-
-total_infections = current_hosp / Penn_market_share / hosp_rate
-detection_prob = initial_infections / total_infections
 
 st.title("COVID-19 Hospital Impact Model for Epidemics")
 st.markdown(
     """*This tool was developed by the [Predictive Healthcare team](http://predictivehealthcare.pennmedicine.org/) at
 Penn Medicine. For questions and comments please see our
-[contact page](http://predictivehealthcare.pennmedicine.org/contact/). Code can be found on [Github](https://github.com/pennsignals/chime). 
+[contact page](http://predictivehealthcare.pennmedicine.org/contact/). Code can be found on [Github](https://github.com/pennsignals/chime).
 Join our [Slack channel](https://codeforphilly.org/chat?channel=covid19-chime-penn) if you would like to get involved!*""")
 
 st.markdown(
-    """The estimated number of currently infected individuals is **{total_infections:.0f}**. The **{initial_infections}** 
-confirmed cases in the region imply a **{detection_prob:.0%}** rate of detection. This is based on current inputs for 
-Hospitalizations (**{current_hosp}**), Hospitalization rate (**{hosp_rate:.0%}**), Region size (**{S}**), 
-and Hospital market share (**{Penn_market_share:.0%}**).""".format(
+    """The **{detection_prob:.0%}** rate of detection and **{initial_infections}** confirmed cases imply a
+    total number of cases of **{total_infections:.0f}**.""".format(
         total_infections=total_infections,
-        current_hosp=current_hosp,
-        hosp_rate=hosp_rate,
-        S=S,
-        Penn_market_share=Penn_market_share,
         initial_infections=initial_infections,
         detection_prob=detection_prob,
     )
@@ -97,7 +91,7 @@ if st.checkbox("Show more info about this tool"):
         "[Discrete-time SIR modeling](https://mathworld.wolfram.com/SIRModel.html) of infections/recovery"
     )
     st.markdown(
-        """The model consists of individuals who are either _Susceptible_ ($S$), _Infected_ ($I$), or _Recovered_ ($R$). 
+        """The model consists of individuals who are either _Susceptible_ ($S$), _Infected_ ($I$), or _Recovered_ ($R$).
 
 The epidemic proceeds via a growth and decline process. This is the core model of infectious disease spread and has been in use in epidemiology for many years."""
     )
@@ -108,7 +102,7 @@ The epidemic proceeds via a growth and decline process. This is the core model o
     st.latex("R_{t+1} = (\\gamma I_t) + R_t")
 
     st.markdown(
-        """To project the expected impact to Penn Medicine, we estimate the terms of the model. 
+        """To project the expected impact to Penn Medicine, we estimate the terms of the model.
 
 To do this, we use a combination of estimates from other locations, informed estimates based on logical reasoning, and best guesses from the American Hospital Association.
 
@@ -116,7 +110,7 @@ To do this, we use a combination of estimates from other locations, informed est
 ### Parameters
 First, we need to express the two parameters $\\beta$ and $\\gamma$ in terms of quantities we can estimate.
 
-- The $\\gamma$ parameter represents 1 over the mean recovery time in days. Since the CDC is recommending 14 days of self-quarantine, we'll use $\\gamma = 1/14$. 
+- The $\\gamma$ parameter represents 1 over the mean recovery time in days. Since the CDC is recommending 14 days of self-quarantine, we'll use $\\gamma = 1/14$.
 - Next, the AHA says to expect a doubling time $T_d$ of 7-10 days. That means an early-phase rate of growth can be computed by using the doubling time formula:
 """
     )
@@ -281,65 +275,6 @@ census_table = census_table.dropna().astype(int)
 if st.checkbox("Show Projected Census in tabular form"):
     st.dataframe(census_table)
 
-st.subheader("Hospital Capacity")
-st.markdown(
-    "Projected number of hospital resources."
-)
-
-cap_dict = {
-    "hosp": hosp_capacity,
-    "icu": icu_capacity,
-    "vent": vent_capacity,
-}
-
-fig, ax = plt.subplots(1, 1, figsize=(10, 4))
-
-census_dict = {}
-for k, los in los_dict.items():
-    census = cap_dict[k] - (
-        projection_admits.cumsum().iloc[:-los, :]
-        - projection_admits.cumsum().shift(los).fillna(0)
-    ).apply(np.ceil)
-    census_dict[k] = census[k]
-    ax.plot(census.head(plot_projection_days)[k], ".-", label=k + " census")
-    ax.legend(loc=0)
-
-ax.set_xlabel("Days from today")
-ax.grid("on")
-ax.set_ylabel("Number of resources")
-ax.axhline(y=0, color='r', linestyle='-')
-st.pyplot()
-
-census_df = pd.DataFrame(census_dict)
-census_df["day"] = census_df.index
-census_df = census_df[["day", "hosp", "icu", "vent"]]
-
-census_table = census_df[np.mod(census_df.index, 7) == 0].copy()
-census_table.index = range(census_table.shape[0])
-census_table.loc[0, :] = 0
-census_table = census_table.dropna().astype(int)
-
-
-wording_dict = {
-    'hosp': 'Hospital beds',
-    'icu': 'ICU beds',
-    'vent': 'Ventilators'
-}
-
-for k, word in wording_dict.items():
-    try:
-        cap_day = census_df[census_df[k] < 0]['day'].iloc[0]
-        st.markdown(
-            "* {} will reach capacity on day **{}**.".format(word, cap_day)
-        )
-    except Exception as e:
-        print(e)
-
-if st.checkbox("Show Projected Hospital Resources in tabular form"):
-    st.dataframe(census_table)
-
-
-
 st.markdown(
     """**Click the checkbox below to view additional data generated by this simulation**"""
 )
@@ -370,7 +305,7 @@ if st.checkbox("Show Additional Projections"):
 st.subheader("References & Acknowledgements")
 st.markdown(
     """* AHA Webinar, Feb 26, James Lawler, MD, an associate professor University of Nebraska Medical Center, What Healthcare Leaders Need To Know: Preparing for the COVID-19
-* We would like to recognize the valuable assistance in consultation and review of model assumptions by Michael Z. Levy, PhD, Associate Professor of Epidemiology, Department of Biostatistics, Epidemiology and Informatics at the Perelman School of Medicine 
+* We would like to recognize the valuable assistance in consultation and review of model assumptions by Michael Z. Levy, PhD, Associate Professor of Epidemiology, Department of Biostatistics, Epidemiology and Informatics at the Perelman School of Medicine
     """
 )
-st.markdown("© 2020, The Trustees of the University of Pennsylvania")
+st.markdown("Â© 2020, The Trustees of the University of Pennsylvania")
