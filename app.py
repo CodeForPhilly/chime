3128fcf7b2d1daa663a23f93192257fac112f346
Corey Chivers <corey.chivers@uphs.upenn.edu>
2020-03-17 12:37:08 -0400

    Recovery_days format as int in description

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ $$\\beta$$ can be interpreted as the _effective contact rate_:
 
 $\\gamma$ is the inverse of the mean recovery time, in days.  I.e.: if $\\gamma = 1/{recovery_days}$, then the average infection will clear in {recovery_days} days. 
 
-An important descriptive parameter is the _basic reproduction number_, or $R_0$.  This represents the average number of people who will be infected by any given infected person.  When $R_0$ is greater than 1, it means that a disease will grow.  Higher $R_0$'s imply more rapid growth.  It is defined as """.format(recovery_days=recovery_days, c='c'))
+An important descriptive parameter is the _basic reproduction number_, or $R_0$.  This represents the average number of people who will be infected by any given infected person.  When $R_0$ is greater than 1, it means that a disease will grow.  Higher $R_0$'s imply more rapid growth.  It is defined as """.format(recovery_days=int(recovery_days)    , c='c'))
     st.latex("R_0 = \\beta /\\gamma")
 
     st.markdown("""
