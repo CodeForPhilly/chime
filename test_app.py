8699e123d800785117acc44cacc04ec5d840bc59
Sam Havens <sam.havens@qordoba.com>
2020-03-18 22:16:24 -0700

    moar tests

--- test_app.py
+++ test_app.py
@@ -5,6 +5,9 @@ from penn_chime.presentation import display_header
 
 
 # set up
+
+# we just want to verify that st _attempted_ to render the right stuff
+# so we store the input, and make sure that it matches what we expect
 class MockStreamlit:
     def __init__(self):
         self.render_store = []
@@ -16,11 +19,19 @@ class MockStreamlit:
         self.render_store.append(inp)
         return None
 
+    def cleanup(self):
+        """
+        Call this after every test, unless you intentionally want to accumulate stuff-to-render
+        """
+        self.render_store = []
+
 
 st = MockStreamlit()
 
 
-def test_header():
+# test presentation
+
+def test_penn_logo_in_header():
     penn_css = '<link rel="stylesheet" href="https://www1.pennmedicine.org/styles/shared/penn-medicine-header.css">'
     display_header(st, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     assert len(
@@ -28,13 +39,27 @@ def test_header():
     ), "The Penn Medicine header should be printed"
 
 
+def test_the_rest_of_header_shows_up():
+    random_part_of_header = "implying an effective $R_t$ of"
+    assert len(
+        list(filter(lambda s: random_part_of_header in s, st.render_store))
+    ), "The whole header should render"
+
+st.cleanup()
+
+
 @pytest.mark.xfail()
 def test_header_fail():
+    """
+    Just proving to myself that these tests work
+    """
     some_garbage = 'ajskhlaeHFPIQONOI8QH34TRNAOP8ESYAW4'
     display_header(st, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     assert len(
         list(filter(lambda s: some_garbage in s, st.render_store))
     ), "This should fail"
+    st.cleanup()
+
 
 # Test the math
 
