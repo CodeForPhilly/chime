f68257a52584a11e2ec3a718b574d1410fba2666
Quinn Dougherty <quinn.dougherty92@gmail.com>
2020-03-21 18:33:16 -0400

    getting tests to work

--- src/test_app.py
+++ src/test_app.py
@@ -7,7 +7,8 @@ import numpy as np
 from app import (projection_admits, alt)
 from penn_chime.models import sir, sim_sir, sim_sir_df
 from penn_chime.parameters import Parameters
-from penn_chime.presentation import display_header, new_admissions_chart
+from penn_chime.presentation import display_header
+from penn_chime.charts import new_admissions_chart
 from penn_chime.settings import DEFAULTS
 from penn_chime.defaults import RateLos
 
