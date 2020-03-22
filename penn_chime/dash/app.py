172885539deddab981120e7323265d0c5ed95a5c
Chris <chriskanii@gmail.com>
2020-03-22 12:16:00 +0100

    Started to replicated streamlit

--- penn_chime/dash/app.py
+++ penn_chime/dash/app.py
@@ -8,23 +8,30 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
 
-from penn_chime.dash.utils import df_to_html_table, get_md_templates
+from penn_chime.dash.utils import (
+    df_to_html_table,
+    get_md_templates,
+    get_yml_templates,
+    render_yml,
+)
 from penn_chime.dash.plotting import get_figure_data
 from penn_chime.dash.tmp_data import DF
 
-EXTERNAL_STYLESHEETS = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+EXTERNAL_STYLESHEETS = [
+    "https://www1.pennmedicine.org/styles/shared/penn-medicine-header.css"
+]
 LANGUAGE = "en"
-
 MD_TEMPLATES = get_md_templates()
+YML_TEMPLATES = get_yml_templates()
 
 APP = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
 APP.layout = Div(
     children=[
-        H1(children="Hello Dash"),
-        Div(children="Dash: A web application framework for Python."),
+        *render_yml(YML_TEMPLATES[LANGUAGE]["header.yml"]),
+        Markdown(MD_TEMPLATES[LANGUAGE]["intro.md"]),
         Graph(id="example-graph"),
         Slider(id="y-max", min=0, max=4, value=1, step=0.5),
-        Markdown(MD_TEMPLATES[LANGUAGE]["table-intro.md"]),
+        Markdown(MD_TEMPLATES[LANGUAGE]["intro.md"]),
         df_to_html_table(DF),
     ]
 )
