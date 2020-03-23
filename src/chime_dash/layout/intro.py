7e5f86d369eb854231982825fd7c0d64b1f5ee61
ckoerber <software@ckoerber.com>
2020-03-23 16:04:19 +0100

    Added new intro section

--- src/chime_dash/layout/intro.py
+++ src/chime_dash/layout/intro.py
@@ -1,32 +1,43 @@
 """Initializes the  dash html
 """
-from typing import List, Any
+from typing import List, Any, Dict
 
 from dash.dependencies import Output
 from dash.development.base_component import ComponentMeta
 from dash_core_components import Markdown
 
+from penn_chime.defaults import Constants
 from penn_chime.models import Parameters
 
 from chime_dash.utils import read_localization_markdown
 
-LOCALIZATION_FILE = "intro.md"
+LOCALIZATION_FILE_1 = "intro.md"
+LOCALIZATION_FILE_2 = "tool-details.md"
 
 
 def setup(language: str) -> List[ComponentMeta]:  # pylint: disable=W0613
     """Initializes the header dash html
     """
 
-    return [Markdown(id="intro")]
+    return [
+        Markdown(id="intro", dangerously_allow_html=True, dedent=True),
+        Markdown(id="more-intro", dangerously_allow_html=True),
+    ]
 
 
-CALLBACK_OUTPUTS = [Output(component_id="intro", component_property="children")]
+CALLBACK_OUTPUTS = [
+    Output(component_id="intro", component_property="children"),
+    Output(component_id="more-intro", component_property="children"),
+]
 
 
-def render(language: str, pars: Parameters) -> List[Any]:
+def render(
+    language: str, pars: Parameters, kwargs: Dict[str, Any], defaults: Constants
+) -> List[Any]:
     """Renders the parameter dependent values in the introduction markdown
     """
-    content = read_localization_markdown(LOCALIZATION_FILE, language)
+    intro = read_localization_markdown(LOCALIZATION_FILE_1, language)
+    tool_details = read_localization_markdown(LOCALIZATION_FILE_2, language)
     detection_prob_str = (
         "{detection_probability:.0%}".format(
             detection_probability=pars.detection_probability
@@ -34,20 +45,39 @@ def render(language: str, pars: Parameters) -> List[Any]:
         if pars.detection_probability is not None
         else "?"
     )
+
+    intro_kwargs = dict(
+        total_infections=pars.infected,
+        initial_infections=pars.known_infected,
+        detection_prob_str=detection_prob_str,
+        current_hosp=pars.current_hospitalized,
+        hosp_rate=pars.hospitalized.rate,
+        S=pars.susceptible,
+        market_share=pars.market_share,
+        recovery_days=pars.recovery_days,
+        r_naught=pars.r_naught,
+        doubling_time=pars.doubling_time,
+        relative_contact_rate=pars.relative_contact_rate,
+        r_t=pars.r_t,
+        doubling_time_t=pars.doubling_time_t,
+    )
+
+    regions = "- " + "| \n".join(
+        f"{key} = {value} "
+        for key, value in defaults.region.__dict__.items()
+        if key != "_s"
+    )
+    tool_kwargs = dict(
+        regions=regions,
+        recovery_days=int(pars.doubling_time),
+        doubling_time=pars.doubling_time,
+        r_naught=pars.r_naught,
+        relative_contact_rate=pars.relative_contact_rate,
+        doubling_time_t=pars.doubling_time_t,
+        r_t=pars.r_t,
+    )
+
     return (
-        content.format(
-            total_infections=pars.infected,
-            initial_infections=pars.known_infected,
-            detection_prob_str=detection_prob_str,
-            current_hosp=pars.current_hospitalized,
-            hosp_rate=pars.hospitalized.rate,
-            S=pars.susceptible,
-            market_share=pars.market_share,
-            recovery_days=pars.recovery_days,
-            r_naught=pars.r_naught,
-            doubling_time=pars.doubling_time,
-            relative_contact_rate=pars.relative_contact_rate,
-            r_t=pars.r_t,
-            doubling_time_t=pars.doubling_time_t,
-        ),
+        intro.format(**intro_kwargs),
+        tool_details.format(**tool_kwargs) if kwargs["show_tool_details"] else "",
     )
