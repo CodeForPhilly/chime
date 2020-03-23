8ef5611314ee28c04f165efec6b73799728f5bcd
ckoerber <software@ckoerber.com>
2020-03-23 11:26:28 +0100

    Removed not needed utils functions

--- src/chime_dash/utils.py
+++ src/chime_dash/utils.py
@@ -1,18 +1,14 @@
 """Utility functions for dash frontend
 """
-from typing import Dict, List, Any
+from typing import Dict, Any
 
-from os import path, walk, sep
+from os import path
 
 from yaml import safe_load
 
 from pandas import DataFrame
 
 from dash_html_components import Table, Thead, Tbody, Tr, Td, Th
-from dash_html_components import H1, H2, H3, H4, H5, H6, Div, A, P, B, I
-from dash_core_components import Slider, Markdown, Graph
-
-import dash_bootstrap_components as dbc
 
 TEMPLATE_DIR = path.join(path.abspath(path.dirname(__file__)), "templates")
 
@@ -30,7 +26,7 @@ def read_localization_yaml(file: str, language: str) -> Dict[str, Any]:
     file_address = path.join(TEMPLATE_DIR, language, file)
     if not path.exists(file_address):
         raise KeyError(
-            "No template found for langage '{language}' and section 'file'".format(
+            "No template found for langage '{language}' and section '{file}'".format(
                 file=file, language=language
             )
         )
@@ -53,7 +49,7 @@ def read_localization_markdown(file: str, language: str) -> str:
     file_address = path.join(TEMPLATE_DIR, language, file)
     if not path.exists(file_address):
         raise KeyError(
-            "No template found for langage '{language}' and section 'file'".format(
+            "No template found for langage '{language}' and section '{file}'".format(
                 file=file, language=language
             )
         )
@@ -61,31 +57,6 @@ def read_localization_markdown(file: str, language: str) -> str:
         md = stream.read()
 
     return md
-
-
-DASH_HTML_ELEMENTS = {
-    "div": Div,
-    "h1": H1,
-    "h2": H2,
-    "h3": H3,
-    "h4": H4,
-    "h5": H5,
-    "h6": H6,
-    "a": A,
-    "p": P,
-    "b": B,
-    "i": I,
-    "markdown": Markdown,
-    "graph": Graph,
-    "slider": Slider,
-    "input": dbc.Input,
-    "label": dbc.Label,
-    "checklist": dbc.Checklist,
-    "formtext": dbc.FormText,
-    "formgroup": dbc.FormGroup,
-    "row": dbc.Row,
-    "col": dbc.Col,
-}
 
 
 def df_to_html_table(dataframe: DataFrame) -> Table:
@@ -102,63 +73,3 @@ def df_to_html_table(dataframe: DataFrame) -> Table:
             ),
         ],
     )
-
-
-def get_md_templates() -> Dict[str, Dict[str, str]]:
-    """Reads all the templates located in the template dir (markdown)
-
-    File names are keys, values are the file content.
-    """
-    templates = dict()
-    for root, _, files in walk(TEMPLATE_DIR):
-        for f in files:
-            if f.endswith("md"):
-                with open(path.join(root, f), "r") as inp:
-                    templates.setdefault(root.split(sep)[-1], dict())[f] = inp.read()
-
-    return templates
-
-
-def get_yml_templates() -> Dict[str, Dict[str, List[Dict[str, Any]]]]:
-    """Finds all the templates located in the template dir (yaml)
-
-    File names are keys, values are the file address.
-    """
-    templates = dict()
-    for root, _, files in walk(TEMPLATE_DIR):
-        for f in files:
-            if f.endswith("yml"):
-                with open(path.join(root, f), "r") as stream:
-                    templates.setdefault(root.split(sep)[-1], dict())[f] = safe_load(
-                        stream
-                    )
-
-    return templates
-
-
-def render_yml(yaml_list: List[Dict[str, Any]]) -> List["HtmlObjtects"]:
-    """Reads yaml file and converts to dash html objects
-    """
-    return [_render_yml(yaml) for yaml in yaml_list]
-
-
-def _render_yml(yaml: Dict[str, str]) -> "HtmlObjtects":
-    """Reads recursively reads dictionary and renders Dash HTML compenents
-    """
-    tmp = dict(**yaml)
-    el = DASH_HTML_ELEMENTS[tmp.pop("element")]
-
-    kwargs = dict()
-    for key, val in tmp.items():
-        # Recursively cast children if possible
-        if key == "children" and isinstance(val, List):
-            children = []
-            for child in val:
-                children.append(_render_yml(child))
-            val = children
-        elif key == "class":
-            key = "className"
-        # Store element creation args
-        kwargs[key] = val
-
-    return el(**kwargs)
