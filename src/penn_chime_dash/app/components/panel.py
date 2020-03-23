953299b2ba74897e8d19ce0aad4a9b248d7d41ee
Brian Ross <ML@BrianThomasRoss.com>
2020-03-23 15:20:31 -0400

    Update Panel

--- src/penn_chime_dash/app/components/panel.py
+++ src/penn_chime_dash/app/components/panel.py
@@ -3,62 +3,53 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from .widgets import FeatureWidgets
 
-PANEL_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
+# PANEL_STYLE = {
+#     "position": "fixed",
+#     "top": 0,
+#     "left": 0,
+#     "bottom": 0,
+#     "width": "16rem",
+#     "padding": "2rem 1rem",
+#     "background-color": "#f8f9fa",
+# }
 
 place = FeatureWidgets()
 
-horizontal = dbc.Row(
-    [
-        dbc.Col([
-            html.Span(
-                'Current Statistics',
-                className='mr-3'
-            ),
-            dbc.Row([
-                place.current_hospitalized,
-                place.doubling_time,
-                place.relative_contact_rate,
-            ]),
-        ]
-        ),
-        dbc.Col([
-            html.Span(
-                'Severity of Interventions',
-                className='mr-3'
-            ),
-            place.hospitilization_rate,
-            place.icu_rate,
-            place.ventilated_rate,
-        ]
-        ),
-        dbc.Col([
-                html.Span(
-                    'Care Duraton',
-                    className='mr-3'
-                ),
-                place.hospitalized_los,
-                place.icu_los,
-                place.ventilated_los,
-                ]
-                ),
-        dbc.Col([
-                html.Span(
-                    'Regional Stats',
-                    className='mr-3'
-                ),
-                place.market_share,
-                place.current_hospitalized,
-                place.known_infected,
 
-                ]
-                )
-    ]
-)
+horizontal = dbc.Container(dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Current Statistics"),
+                place.widget("current_hospitalized"),
+                place.widget("relative_contact_rate"),
+                place.widget("doubling_time"),
+            ],
+        ),
+        dbc.Col(
+            [
+                html.H6("Outcomes (% total infected)"),
+                place.widget("hospitalization_rate"),
+                place.widget("icu_rate"),
+                place.widget("ventilated_rate"),
+            ]
+        ),
+        dbc.Col(
+            [
+                html.H6("Care Duration (days)"),
+                place.widget("hospitalized_los"),
+                place.widget("icu_los"),
+                place.widget("ventilated_los"),
+            ]
+        ),
+        dbc.Col(
+            [
+                html.H6("Regional Statistics"),
+                place.widget("known_infected"),
+                place.widget("susceptible"),
+                place.widget("market_share")
+            ],
+        )
+    ],
+
+),
