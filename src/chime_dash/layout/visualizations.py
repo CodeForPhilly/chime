1a8df1f7005b0eaed2e19374e6ba1cae5c37c543
ckoerber <software@ckoerber.com>
2020-03-23 13:01:38 +0100

    Reintroduced visuzalizations

--- src/chime_dash/layout/visualizations.py
+++ src/chime_dash/layout/visualizations.py
@@ -1,12 +1,20 @@
 """Initializes the  dash html
 """
-from typing import List
+from typing import List, Any
 
+from dash.dependencies import Output
 from dash.development.base_component import ComponentMeta
+from dash_html_components import H2, Div
+from dash_core_components import Markdown, Graph
+
+from penn_chime.utils import build_census_df, build_admissions_df, add_date_column
+from penn_chime.models import Parameters
+
 
 from chime_dash.utils import read_localization_yaml
+from chime_dash.plotting import plot_dataframe
 
-LOCALIZATION_FILE = ""
+LOCALIZATION_FILE = "visualizations.yml"
 
 
 def setup(language: str) -> List[ComponentMeta]:
@@ -14,4 +22,42 @@ def setup(language: str) -> List[ComponentMeta]:
     """
     content = read_localization_yaml(LOCALIZATION_FILE, language)
 
-    return
+    return [
+        H2(content["new-admissions-title"]),
+        Markdown(content["new-admissions-text"]),
+        Graph(id="new-admissions-graph"),
+        H2(content["admitted-patients-title"]),
+        Markdown(content["admitted-patients-text"]),
+        Graph(id="admitted-patients-graph"),
+    ]
+
+
+CALLBACK_OUTPUTS = [
+    Output(component_id="new-admissions-graph", component_property="figure"),
+    Output(component_id="admitted-patients-graph", component_property="figure"),
+]
+
+
+def render(language: str, pars: Parameters, as_date: bool = False) -> List[Any]:
+    """Renders the parameter dependent plots and tables
+    """
+    content = read_localization_yaml(LOCALIZATION_FILE, language)
+
+    # Prepare admissions data
+    projection_admits = build_admissions_df(pars.n_days, *pars.dispositions)
+    if as_date:
+        projection_admits = add_date_column(
+            projection_admits, drop_day_column=True
+        ).set_index("date")
+    else:
+        projection_admits = projection_admits.set_index("day")
+
+    localized_columns = {key: content[key] for key in projection_admits.columns}
+    projection_admits = projection_admits.dropna().rename(columns=localized_columns)
+
+    # Plot admissions figure
+    admissions_data = plot_dataframe(
+        projection_admits.head(pars.n_days - 10), max_y_axis=pars.max_y_axis,
+    )
+
+    return (admissions_data, admissions_data)
--- src/chime_dash/plotting.py
+++ src/chime_dash/plotting.py
@@ -7,26 +7,17 @@ from penn_chime.utils import add_date_column
 import pandas as pd
 
 
-def new_admissions_chart(
-    projection_admits: pd.DataFrame,
-    plot_projection_days: int,
-    as_date: bool = False,
-    max_y_axis: int = None,
-) -> Dict[str, Any]:
+def plot_dataframe(dataframe: pd.DataFrame, max_y_axis: int = None,) -> Dict[str, Any]:
     """
     """
-    projection_admits = projection_admits.rename(
-        columns={"hosp": "Hospitalized", "icu": "ICU", "vent": "Ventilated"}
-    )
-    projection_admits = add_date_column(projection_admits).dropna()
     return {
         "data": [
             {
-                "x": projection_admits["date"] if as_date else projection_admits["day"],
-                "y": projection_admits[col].astype(int),
+                "x": dataframe.index,
+                "y": dataframe[col].astype(int),
                 "name": col,
                 "mode": "lines+markers",
             }
-            for col in ["Hospitalized", "ICU", "Ventilated"]
+            for col in dataframe.columns
         ]
     }
