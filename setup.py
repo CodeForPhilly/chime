cb3338e993d6ab6156aed8b51f900362990c72f1
Nick Canzoneri <nickcanz@github.com>
2020-03-25 11:14:50 -0400

    Add a setup.py and move the cli file so that it is easily installed by the package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="penn_chime",
+    version="1.0.0",
+    author="Predictive Healthcare @ Penn Medicine",
+    description="COVID-19 Hospital Impact Model for Epidemics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CodeForPhilly/chime",
+    package_dir={'': 'src'},
+    packages=setuptools.find_namespace_packages(where='src', exclude=('tests')),
+    install_requires=['pandas','numpy','altair','streamlit'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points = {
+        'console_scripts': ['penn_chime=penn_chime.cli:main'],
+    }
+)
