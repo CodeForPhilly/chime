6e4b2d236c39cbd2176915dfd462372372c193a8
Jason Lubken <jlubken@users.noreply.github.com>
2020-03-29 12:51:55 -0400

    Off by one for cypress

--- e2e/cypress/integration/tests/actions.spec.js
+++ e2e/cypress/integration/tests/actions.spec.js
@@ -10,7 +10,7 @@ context('Actions', () => {
 
     // This gets the "first" input from the sidebar. From clicking step up,
     // the Regional Population should increase from default 4119405 to 4219405.
-    cy.get('input.st-al').eq(2)
+    cy.get('input.st-al').eq(1)
       .should('has.value', '4119406')
   })
 });
