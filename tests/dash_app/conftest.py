c36e0b07d73142b1b68f9d337b6bbb2cd4ab91f4
Akim Akimov <zedmor@gmail.com>
2020-03-30 11:22:27 -0400

    Add conftest for UI testing

---
+++ tests/dash_app/conftest.py
@@ -0,0 +1,22 @@
+from pytest import fixture
+from selenium.webdriver.chrome.options import Options
+
+from dash_app import app
+
+
+def pytest_setup_options():
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-extensions")
+
+    return options
+
+
+@fixture
+def test_app(dash_duo):
+
+    dash_duo.start_server(app)
+
+    yield dash_duo
